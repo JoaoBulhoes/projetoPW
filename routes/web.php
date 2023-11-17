cfaf84f4182974413c4d5d9381bafdca730880ce
PedroBotelho <pedro.botelho2151@gmail.com>
2023-11-17 13:32:38 -0100

    Created FileController New routes for FileController New File Model

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/files', 'FileController@index')->name('files.index');
+Route::post('/files/search', 'FileController@search')->name('files.search');
+
