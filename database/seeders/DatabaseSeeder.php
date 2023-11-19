814f7fc81f76b2271fef39fa649d5ab3a9674d4e
JoaoBulhoes <107730301+JoaoBulhoes@users.noreply.github.com>
2023-11-19 18:34:22 -0100

    alterando o seeder para apenas criar dados quando a aplicaçao tem a variavel environment (no ficheiro .env) igual a 'local'

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,15 +5,14 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Department;
 use App\Models\Document;
-use App\Models\DocumentPermission;
 use App\Models\FileLink;
-use App\Models\Metadata;
 use App\Models\MetadataType;
 use App\Models\Permission;
 use App\Models\Profile;
 use App\Models\Revision;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\App;
 
 class DatabaseSeeder extends Seeder
 {
@@ -29,13 +28,15 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        User::factory(30)->create();
-        Document::factory(30)->create();
-        Revision::factory(30)->create();
-        Permission::factory(30)->create();
-        Department::factory(30)->create();
-        Profile::factory(30)->create();
-        FileLink::factory(30)->create();
-        MetadataType::factory(30)->create();
+        if (App::environment() == 'local') {
+            User::factory(30)->create();
+            Document::factory(30)->create();
+            Revision::factory(30)->create();
+            Permission::factory(30)->create();
+            Department::factory(30)->create();
+            Profile::factory(30)->create();
+            FileLink::factory(30)->create();
+            MetadataType::factory(30)->create();
+        }
     }
 }
