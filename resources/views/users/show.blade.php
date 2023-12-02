3a1754fd449fcd055ac1adcb73539a7eeadb9408
JoaoBulhoes <107730301+JoaoBulhoes@users.noreply.github.com>
2023-12-02 00:22:48 -0100

    Melhorando a aparencia da view users.show

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -2,7 +2,7 @@
 
 @section('main-content')
 
-    <h1>Ficha de funcionario</h1>
+    <h1 class="text-md-center" style="color: white">Utilizadores</h1>
 
     <div class="row">
         <div class="col">
@@ -11,7 +11,25 @@
                     Dados Gerais
                 </div>
                 <card-body>
-                    ola {{ $user->name }}
+                    <table class="table table-striped table-hover">
+                        <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Departments</th>
+                        </tr>
+                        </thead>
+
+                        <tbody>
+                        <tr>
+                            <td>{{ $user->name }}</td>
+                            <td>
+                                @foreach($user->departments as $department)
+                                    <p>{{ $department->name }}</p>
+                                @endforeach
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
                 </card-body>
             </div>
         </div>
