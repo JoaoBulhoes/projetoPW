2f2a768f2c9a54893c46a3ac8066264c847b78ef
JoaoBulhoes <107730301+JoaoBulhoes@users.noreply.github.com>
2023-12-18 22:06:30 -0100

    Acesso aos users por api:
    - podemos mostrar 5 utilizadores
    - podemos mostrar 1 utilizador em especifico
    - podemos apagar utilizadores

---
+++ app/Http/Controllers/api/UserApiController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
+use App\Http\Resources\UserResourceCollection;
+use App\Models\User;
+use App\Services\UserService;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UserApiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        if (!Auth::user()->tokenCan('users:index')) {
+            return response()->json(['message' => 'FORBIDDEN'], 403);
+        }
+
+        return new UserResourceCollection(User::paginate(5));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        if (!auth()->user()->tokenCan('users:show')) {
+            return response()->json(['message' => 'FORBIDDEN'], 403);
+        }
+
+        try {
+            $user = User::findOrFail($id);
+            return new userResource($user);
+        } catch (\Exception $e) {
+            if ($e instanceof ModelNotFoundException) {
+                return response()->json(['message' => 'Não encontrado'], 404);
+            }
+
+            return response()->json(['message' => 'Ocorreu um erro de comunicação'], 503);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        return response()->json($request->toArray());
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        if (!auth()->user()->tokenCan('users:delete')) {
+            return response()->json(['message' => 'FORBIDDEN'], 403);
+        }
+
+        try {
+            $user = User::findOrFail($id);
+            $user->delete();
+        } catch (\Exception $e) {
+            if ($e instanceof ModelNotFoundException) {
+                return response()->json(['message' => 'Não encontrado'], 404);
+            }
+
+            return response()->json(['message' => 'Ocorreu um erro de comunicação'], 503);
+        }
+
+        return response()->json(['message' => 'Apagado com sucesso'], 200);
+    }
+}
