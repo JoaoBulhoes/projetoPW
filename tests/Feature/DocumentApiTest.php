d7c36d3ea60c488ae2bbb06399008c0221ae12f8
JoaoBulhoes <107730301+JoaoBulhoes@users.noreply.github.com>
2024-02-06 00:26:51 -0100

    Pequena correcao no teste api Documents

--- tests/Feature/DocumentApiTest.php
+++ tests/Feature/DocumentApiTest.php
@@ -153,7 +153,6 @@ class DocumentApiTest extends TestCase
             [
                 'Accept' => 'application/json',
                 'Authorization' => 'Bearer ' . $token,
-                'name' => 'api name'
             ]
         );
         $response->assertStatus(403);
