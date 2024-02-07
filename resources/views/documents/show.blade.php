237c4927e86faa8cce4fa17c869d24175b6c5566
JoaoBulhoes <107730301+JoaoBulhoes@users.noreply.github.com>
2024-02-07 15:08:59 -0100

    ver conteudos do documento

--- resources/views/documents/show.blade.php
+++ resources/views/documents/show.blade.php
@@ -23,19 +23,36 @@
                         <tbody>
                         <tr>
                             <td>{{ $document->path }}</td>
-                                <td>
-                                    @for($i = 0; $i < $metadataTypeInfo->count(); $i++)
-                                        <p>{{ $metadataTypeInfo[$i]["name"] }}</p>
-                                    @endfor
-                                </td>
-                                <td>
-                                    @for($i = 0; $i < $metadataTypeInfo->count(); $i++)
-                                        <p>{{ $metadataTypeInfo[$i]["value"] }}</p>
-                                    @endfor
-                                </td>
+                            <td>
+                                @for($i = 0; $i < $metadataTypeInfo->count(); $i++)
+                                    <p>{{ $metadataTypeInfo[$i]["name"] }}</p>
+                                @endfor
+                            </td>
+                            <td>
+                                @for($i = 0; $i < $metadataTypeInfo->count(); $i++)
+                                    <p>{{ $metadataTypeInfo[$i]["value"] }}</p>
+                                @endfor
+                            </td>
                         </tr>
                         </tbody>
                     </table>
+                    <div style="width: 100%">
+                        @php
+                            if (\Illuminate\Support\Facades\Storage::exists($document->path)) {
+                                $content = \Illuminate\Support\Facades\Storage::get($document->path);
+                                $type = \Illuminate\Support\Facades\Storage::mimeType($document->path);
+                            } else {
+                                $content = 'O documento solicitado não existe.';
+                                $type = '';
+                            }
+                        @endphp
+
+                        @if (Str::startsWith($type, 'image/'))
+                            <img src="data:{{ $type }};base64,{{ base64_encode($content) }}" width="100%"/>
+                        @else
+                            <pre style="height: 500px">{{ $content }}</pre>
+                        @endif
+                    </div>
                 </card-body>
             </div>
         </div>
